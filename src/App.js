1f9ae5d638ab59a2d9710df94d554795aeae2347
degi100 <r.degering@mail.de>
2021-10-02 13:49:52 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
-
+import Header from './components/layout/Header';
+import Bottom from './components/layout/Bottom';
 function App() {
   return (
     <div className="App">
-      
+     <Header /> 
+     <Bottom />
     </div>
   );
 }
---
+++ src/components/layout/Bottom.jsx
@@ -0,0 +1,9 @@
+const Bottom = () => {
+  return (
+    <div>
+      <h2>Bottom</h2>
+    </div>
+  );
+};
+
+export default Bottom;

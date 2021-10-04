c26a4f815a4cf829be8394d8110deca2b0896625
degi100 <r.degering@mail.de>
2021-10-04 20:45:50 +0200

    searchbar is running

--- src/components/vim/vim.jsx
+++ src/components/vim/vim.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import initialVim from "../../data/vim.json";
 // import ReactPlayer from "react-player";
 import "../../styles/vim.scss";
 
 const Vim = () => {
   const [searchText, setSearchText] = useState("");
-  const [vimCommands, setVimCommands] = useState(initialVim);
-
-  const searchCommands = (userSearchText) => {
-    setSearchText(userSearchText);
-  };
-
-  const toggleDescription = (index) => {
-    vimCommands[index].show = !vimCommands[index].show;
-    setVimCommands([...vimCommands]);
-  };
+  const [displayVimCommands, setDisplayVimCommands] = useState(initialVim);
+  const [initialVimCommands] = useState(initialVim);
+  useEffect(() => {
+    setDisplayVimCommands(
+      initialVimCommands.filter((vimCommand) =>
+        vimCommand.title.toLowerCase().includes(searchText.toLowerCase())
+      )
+    );
+  }, [searchText]);
 
   const vimCommandContainsSearchText = (vimCommand) => {
     return vimCommand.title.toLowerCase().includes(searchText.toLowerCase());
+  };
+
+  const changeSearchText = (e) => {
+    setSearchText(e.target.value);
   };
 
   return (
@@ -26,16 +29,17 @@ const Vim = () => {
         type="text"
         className="inputBox"
         placeholder="Search for Command"
+        onChange={changeSearchText}
       />
 
       <p className="count-all">
-        There are {vimCommands.length} commands and{" "}
-        {vimCommands.filter((vimCommand) => vimCommand.title).length} are
+        There are {displayVimCommands.length} commands and{" "}
+        {displayVimCommands.filter((vimCommand) => vimCommand.title).length} are
         showing.
       </p>
 
       <div className="vimninjabelts">
-        {initialVim.map((e) => {
+        {displayVimCommands.map((e) => {
           return (
             <div className={`vimTitle`}>
               <ul>

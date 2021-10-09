233f6dc1f25c848d14c0813b75e176ebe9ae5f4e
degi100 <r.degering@mail.de>
2021-10-09 02:04:09 +0200

    fix problem

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import Combo from "../pages/Combo";

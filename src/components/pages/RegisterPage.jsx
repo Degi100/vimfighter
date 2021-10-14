eba7be9912d3bb8a2b241d157ad5580834678fe2
degi100 <r.degering@mail.de>
2021-10-14 09:28:24 +0200

    style register

--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -2,12 +2,17 @@ import showVimCommands from "../../data/vim.json";
 import "../../styles/register.scss";
 
 const Register = () => {
+  const convertToClassName = (text) => {
+      return text.replaceAll(" ", "").toLowerCase();
+  }
+
+  
   return (
     <div className="panel">
       <ul>
         {showVimCommands.map((vimCommand, index) => {
           return (
-            <li className="content" key={index}>
+            <li className={`content ${convertToClassName(vimCommand.category)}`} key={index}>
               <p className="vim-category">{vimCommand.category}</p>
               <p className="vim-title">{vimCommand.title}</p>
               <p className="vim-commands">{vimCommand.command}</p>
